bf18cf2b82c122dbc8fb83c6a08e4dacf7213b16
mehm8128 <mehm8128@gmail.com>
2021-12-30 15:50:34 +0900

    init

---
+++ go.mod
@@ -0,0 +1,6 @@
+module github.com/mehm8128/herokuPractice
+
+go 1.16
+// +heroku goVersion go1.16
+
+require github.com/labstack/echo/v4 v4.6.1
---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"net/http"
+	"os"
+
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	e := echo.New()
+
+	e.GET("/hello", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World.\n")
+	})
+    e.Logger.Fatal(e.Start(":"+os.Getenv("PORT"))) // ここを前述の通り自分のポートにすること(例: e.Start(":10100"))
+}
