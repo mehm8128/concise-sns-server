da5e65698e7d3c2dbb5480bbb829de0e25afa343
mehm8128 <mehm8128@gmail.com>
2021-12-31 17:17:40 +0900

    おみくじ機能搭載

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"math/rand"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/labstack/echo/v4"
 )
@@ -10,8 +12,29 @@ import (
 func main() {
 	e := echo.New()
 
-	e.GET("/hello", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World.\n")
+	e.GET("/ping", func(c echo.Context) error {
+		return c.String(http.StatusOK, "pong!\n")
 	})
-    e.Logger.Fatal(e.Start(":"+os.Getenv("PORT"))) // ここを前述の通り自分のポートにすること(例: e.Start(":10100"))
+	e.GET("/omikuji", func(c echo.Context) error {
+		rand.Seed(time.Now().UnixNano())
+    omikujiNum:=rand.Intn(6)
+		omikujiResult:=""
+		switch omikujiNum{
+		case 0:
+			omikujiResult="大吉"
+		case 1:
+			omikujiResult="中吉"
+		case 2:
+			omikujiResult="小吉"
+		case 3:
+			omikujiResult="吉"
+		case 4:
+			omikujiResult="凶"
+		case 5:
+			omikujiResult="大凶"
+		}
+
+		return c.String(http.StatusOK, omikujiResult+"\n")
+	})
+    e.Logger.Fatal(e.Start(":"+os.Getenv("PORT")))
 }
