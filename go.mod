df4498b85449314fa1511e644fa7acd3d0349197
mehm8128 <mehm8128@gmail.com>
2022-01-14 12:55:15 +0900

    gormでDB使うやつコピペしてみた

--- go.mod
+++ go.mod
@@ -1,6 +1,16 @@
 module github.com/mehm8128/herokuPractice
 
 go 1.16
+
 // +heroku goVersion go1.16
 
-require github.com/labstack/echo/v4 v4.6.1
+require (
+	github.com/jinzhu/gorm v1.9.16
+	github.com/jinzhu/now v1.1.4 // indirect
+	github.com/labstack/echo v3.3.10+incompatible
+	github.com/labstack/gommon v0.3.1 // indirect
+	github.com/mattn/go-sqlite3 v1.14.10 // indirect
+	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
+	golang.org/x/net v0.0.0-20210913180222-943fd674d43e // indirect
+	golang.org/x/text v0.3.7 // indirect
+)
--- main.go
+++ main.go
@@ -1,40 +1,55 @@
 package main
 
 import (
-	"math/rand"
 	"net/http"
 	"os"
-	"time"
 
-	"github.com/labstack/echo/v4"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+	"github.com/labstack/echo"
+)
+
+type Content struct {
+	gorm.Model
+	Text    string `json:"text"`
+}
+
+// DBのインスタンスをグローバル変数に格納
+var (
+	db *gorm.DB
 )
 
 func main() {
-	e := echo.New()
+    // DB接続処理
+    var err error
+    db, err = gorm.Open("sqlite3", "/tmp/gorm.db")
+    if err != nil {
+        panic("failed to connect database")
+    }
+    // サーバーが終了したらDB接続も終了する
+    defer db.Close()
+    
+    // サーバーのインスタンス作成
+    e := echo.New()
+    
+    // ルーティング設定
+    e.GET("/contents", getAllContents)
+    e.POST("/create", createContent)
+    
+    // サーバー起動
+    e.Logger.Fatal(e.Start(":" + os.Getenv("PORT")))
+}
 
-	e.GET("/ping", func(c echo.Context) error {
-		return c.String(http.StatusOK, "pong!\n")
-	})
-	e.GET("/omikuji", func(c echo.Context) error {
-		rand.Seed(time.Now().UnixNano())
-    omikujiNum:=rand.Intn(6)
-		omikujiResult:=""
-		switch omikujiNum{
-		case 0:
-			omikujiResult="大吉"
-		case 1:
-			omikujiResult="中吉"
-		case 2:
-			omikujiResult="小吉"
-		case 3:
-			omikujiResult="吉"
-		case 4:
-			omikujiResult="凶"
-		case 5:
-			omikujiResult="大凶"
-		}
+func createContent(c echo.Context) error {
+    // レコード登録
+    db.Create(&Content{Text: "newText"})
+    return c.String(http.StatusOK, "record created")
+}
 
-		return c.String(http.StatusOK, omikujiResult+"\n")
-	})
-    e.Logger.Fatal(e.Start(":"+os.Getenv("PORT")))
+func getAllContents(c echo.Context) error {
+    var content Content
+    // contentテーブルのレコードを全件取得
+    db.Find(&content)
+    // 取得したデータをJSONにして返却
+    return c.JSON(http.StatusOK, content)
 }
